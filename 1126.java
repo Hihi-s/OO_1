0d4ace6e7ab5f1b0cea6423b8263f3f8eea644e2
Hihi-s <55428253+Hihi-s@users.noreply.github.com>
2019-11-26 16:19:35 +0800

    Update 1126.java

--- 1126.java
+++ 1126.java
@@ -42,7 +42,7 @@ interface Shape {
    }
 }
 
-class TestShape {
+pubic class TestShape {
    public static void main(String[] args) {
       Shape s1 = new Rectangle(1, 2);  // upcast
       System.out.println(s1);
